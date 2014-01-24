60c78ed4281aecf0ba9a6056f1cab03d3310e4fb
alexai <yai@dfengg.com>
2014-01-24 11:32:45 +0800

    First commit

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default['dfjdk']['artifactory_repo']="libs-snapshot-local"
+default['dfjdk']['artifactory_path']="com/demandforce/3rdparty/jdk/1.6.31-SNAPSHOT"
+default['dfjdk']['rpm_name']="jdk-1.6.31-20120921.174938-1.rpm"
+default['dfjdk']['rpm_version']="jdk-1.6.31-20120921.174938"
+
